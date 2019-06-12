3a922ce6b44a6a39f6b6bf533387d082341f2071
Daniela Ibasca <40763585+DanielaIbasca@users.noreply.github.com>
2019-06-12 20:16:22 -0300

    Update main.c

--- Clase 13/eEmpleado/main.c
+++ Clase 13/eEmpleado/main.c
@@ -11,7 +11,7 @@ int main()
     /*********** TAREA ***********
     -COMPLETAR:
     -LLAMADAS A FUNCION.
-    -ARRARY DE EMPLEADOS.
+    -ARRAY DE EMPLEADOS.
     -MOSTRAR EMPLEADOS.
     *******************************/
 
